550c64e1af1d5b316aa294724e2c65e776d68a35
badPcGames <v.y.shumsjkyy@student.khai.edu>
2024-09-03 15:43:28 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSolve = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSystem = new System.Windows.Forms.TextBox();
+            this.textBoxPriceOP = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxOpRes = new System.Windows.Forms.TextBox();
+            this.textBoxOptiRes = new System.Windows.Forms.TextBox();
+            this.textBoxPriceOpti = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxWant = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxHave = new System.Windows.Forms.TextBox();
+            this.textBoxQueue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSolve
+            // 
+            this.buttonSolve.Location = new System.Drawing.Point(16, 498);
+            this.buttonSolve.Name = "buttonSolve";
+            this.buttonSolve.Size = new System.Drawing.Size(438, 58);
+            this.buttonSolve.TabIndex = 5;
+            this.buttonSolve.Text = "Розрахувати";
+            this.buttonSolve.UseVisualStyleBackColor = true;
+            this.buttonSolve.Click += new System.EventHandler(this.buttonSolve_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Матриця вартості";
+            // 
+            // textBoxSystem
+            // 
+            this.textBoxSystem.Location = new System.Drawing.Point(16, 39);
+            this.textBoxSystem.Multiline = true;
+            this.textBoxSystem.Name = "textBoxSystem";
+            this.textBoxSystem.Size = new System.Drawing.Size(213, 241);
+            this.textBoxSystem.TabIndex = 0;
+            // 
+            // textBoxPriceOP
+            // 
+            this.textBoxPriceOP.Location = new System.Drawing.Point(16, 472);
+            this.textBoxPriceOP.Name = "textBoxPriceOP";
+            this.textBoxPriceOP.Size = new System.Drawing.Size(213, 20);
+            this.textBoxPriceOP.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(14, 456);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 13);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Вартість";
+            // 
+            // textBoxOpRes
+            // 
+            this.textBoxOpRes.Location = new System.Drawing.Point(17, 305);
+            this.textBoxOpRes.Multiline = true;
+            this.textBoxOpRes.Name = "textBoxOpRes";
+            this.textBoxOpRes.Size = new System.Drawing.Size(212, 135);
+            this.textBoxOpRes.TabIndex = 28;
+            // 
+            // textBoxOptiRes
+            // 
+            this.textBoxOptiRes.Location = new System.Drawing.Point(242, 305);
+            this.textBoxOptiRes.Multiline = true;
+            this.textBoxOptiRes.Name = "textBoxOptiRes";
+            this.textBoxOptiRes.Size = new System.Drawing.Size(212, 135);
+            this.textBoxOptiRes.TabIndex = 30;
+            // 
+            // textBoxPriceOpti
+            // 
+            this.textBoxPriceOpti.Location = new System.Drawing.Point(241, 472);
+            this.textBoxPriceOpti.Name = "textBoxPriceOpti";
+            this.textBoxPriceOpti.Size = new System.Drawing.Size(213, 20);
+            this.textBoxPriceOpti.TabIndex = 29;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Вартість";
+            // 
+            // textBoxWant
+            // 
+            this.textBoxWant.Location = new System.Drawing.Point(242, 117);
+            this.textBoxWant.Name = "textBoxWant";
+            this.textBoxWant.Size = new System.Drawing.Size(202, 20);
+            this.textBoxWant.TabIndex = 32;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(239, 101);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(44, 13);
+            this.label.TabIndex = 33;
+            this.label.Text = "Заявки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Запаси";
+            // 
+            // textBoxHave
+            // 
+            this.textBoxHave.Location = new System.Drawing.Point(242, 60);
+            this.textBoxHave.Name = "textBoxHave";
+            this.textBoxHave.Size = new System.Drawing.Size(202, 20);
+            this.textBoxHave.TabIndex = 34;
+            // 
+            // textBoxQueue
+            // 
+            this.textBoxQueue.Location = new System.Drawing.Point(242, 243);
+            this.textBoxQueue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxQueue.Multiline = true;
+            this.textBoxQueue.Name = "textBoxQueue";
+            this.textBoxQueue.Size = new System.Drawing.Size(202, 37);
+            this.textBoxQueue.TabIndex = 36;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 13);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Базові клітинки що треба змінити";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Опорний план";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(239, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Оптимальний план";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 568);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxQueue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxHave);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBoxWant);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOptiRes);
+            this.Controls.Add(this.textBoxPriceOpti);
+            this.Controls.Add(this.textBoxOpRes);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxPriceOP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSolve);
+            this.Controls.Add(this.textBoxSystem);
+            this.Name = "Form1";
+            this.Text = "LR8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSolve;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSystem;
+        private System.Windows.Forms.TextBox textBoxPriceOP;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxOpRes;
+        private System.Windows.Forms.TextBox textBoxOptiRes;
+        private System.Windows.Forms.TextBox textBoxPriceOpti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxWant;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxHave;
+        private System.Windows.Forms.TextBox textBoxQueue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StreamWriter sw;
+        private int[,] priceMatrix;
+        private int[,] res;
+        private int[] want;
+        private int[] have;
+        int sizeX;
+        int sizeY;
+
+
+        private void buttonSolve_Click(object sender, EventArgs e)
+        {
+            sw = new StreamWriter(@"D:\result.txt");
+            makeMatrix();
+            solve();
+            sw.Close();
+        }
+        private void makeMatrix()
+        {
+            string[] equlense = textBoxSystem.Text.Split('\n');
+            string[] line = equlense[0].Split(' ');
+            priceMatrix = new int[line.Length, equlense.Length];
+            for (int j = 0; j < equlense.Length; j++)
+            {
+                line = equlense[j].Split(' ');
+                for (int i = 0; i < line.Length; i++)
+                {
+                    priceMatrix[i, j] = int.Parse(line[i]);
+                }
+            }
+        }
+        private void fileWrite(int[,] m, int sizeX, int sizeY)
+        {
+            string line = "";
+            line += "_______________________\n";
+            for (int j = 0; j < sizeY; j++)
+            {
+                for (int i = 0; i < sizeX; i++)
+                {
+                    line += " " + m[i, j] + " ";
+
+                }
+                line += "\n";
+            }
+
+            sw.WriteLine(line);
+
+        }
+
+
+        void opSolveMinimumElements()
+        {
+            int[] copyWant = want;
+            int[] copyHave = have;
+            bool isEmpty;
+            int mustBeBigger = 0;
+            do
+            {
+                isEmpty = true;
+                int min = int.MaxValue;
+                for (int i = 0; i < copyHave.Length; i++)
+                {
+                    for (int j = 0; j < copyWant.Length; j++)
+                    {
+                        if (priceMatrix[j, i] > mustBeBigger && priceMatrix[j, i] < min)
+                        {
+                            min = priceMatrix[j, i];
+                        }
+                    }
+                }
+                mustBeBigger = min;
+                for (int i = 0; i < copyHave.Length; i++)
+                {
+                    for (int j = 0; j < copyWant.Length; j++)
+                    {
+                        int change = 0;
+                        if (priceMatrix[j, i] == min)
+                        {
+                            if (copyWant[j] >= copyHave[i])
+                            {
+                                res[j, i] = copyHave[i];
+                                change = copyHave[i];
+                                copyHave[i] -= change;
+                                copyWant[j] -= change;
+                            }
+                            else
+                            {
+                                res[j, i] = copyWant[j];
+                                change = copyWant[j];
+                                copyHave[i] -= change;
+                                copyWant[j] -= change;
+                            }
+                        }
+                    }
+                }
+                foreach (int el in copyHave)
+                {
+                    if (el != 0) isEmpty = false;
+                }
+
+                foreach (int el in copyWant)
+                {
+                    if (el != 0) isEmpty = false;
+                }
+
+            } while (!isEmpty);
+
+
+        }
+
+        void solve()
+        {
+            sw.WriteLine("Матриця вартостей");
+            sizeX = priceMatrix.GetLength(0);
+            sizeY = priceMatrix.Length / priceMatrix.GetLength(0);
+            have = new int[sizeY];
+            res = new int[sizeX, sizeY];
+            string[] h = textBoxHave.Text.Split(' ');
+            for (int i = 0; i < h.Length; i++)
+            {
+                have[i] = int.Parse(h[i]);
+            }
+            sw.WriteLine("Вектор запасів: "+textBoxHave.Text);
+            want = new int[sizeX];
+            string[] w = textBoxWant.Text.Split(' ');
+            for (int i = 0; i < w.Length; i++)
+            {
+                want[i] = int.Parse(w[i]);
+            }
+            sw.WriteLine("Вектор заявок: " + textBoxWant.Text);
+            fileWrite(priceMatrix, sizeX, sizeY);
+            sw.WriteLine("Пошук опорного плану перевезень методом методом мінімального елемента");
+            opSolveMinimumElements();
+            int price = 0;
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    price += priceMatrix[i, j] * res[i, j];
+                }
+            }
+            string line = "";
+            for (int j = 0; j < sizeY; j++)
+            {
+                for (int i = 0; i < sizeX; i++)
+                {
+                    if (res[i, j] == 0)
+                    {
+                        line += " " + "x" + " ";
+                    }
+                    else
+                    {
+                        line += " " + res[i, j] + " ";
+                    }
+
+                }
+                line += "\r\n";
+            }
+            textBoxOpRes.Text = line;
+            sw.WriteLine("Опорний план перевезення");
+            sw.WriteLine(line);
+            sw.WriteLine("Ціна= " + price);
+            textBoxPriceOP.Text = price.ToString();
+            sw.WriteLine("Пошук оптимального плану перевезень методом потенціалів:");
+            potencial();
+            line = "";
+            for (int j = 0; j < sizeY; j++)
+            {
+                for (int i = 0; i < sizeX; i++)
+                {
+                    line += " " + res[i, j] + " ";
+                }
+                line += "\r\n";
+            }
+            textBoxOptiRes.Text = line;
+            price = 0;
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    price += priceMatrix[i, j] * res[i, j];
+                }
+            }
+            sw.WriteLine("Оптимальний план перевезень:");
+            fileWrite(res, sizeX, sizeY);
+            sw.WriteLine("Ціна= " + price);
+            textBoxPriceOpti.Text = price.ToString();
+        }
+
+        void potencial()
+        {
+            int[,] potencials=new int[sizeX,sizeY];
+            for(int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0;j < sizeY; j++)
+                {
+                    potencials[i, j] = res[i, j];
+                }
+            }
+
+            int?[] potentialWant = new int?[sizeX] ;
+            int?[] potentialHave = new int?[sizeY] ;
+
+
+
+            potentialHave[0] = 0;
+            bool isNull;
+            do
+            {
+                isNull = false;
+                for (int j = 0; j < sizeY; j++)
+                {
+                    for (int i = 0; i < sizeX; i++)
+                    {
+                        if (res[i, j] != 0)
+                        {
+                            if (potentialHave[j] == null)
+                            {
+                                potentialHave[j] = priceMatrix[i, j] - potentialWant[i];
+                            }
+                            if (potentialWant[i] == null)
+                            {
+                                potentialWant[i] = priceMatrix[i, j] - potentialHave[j];
+                            }
+                        }
+                    }
+                }
+                foreach (int? el in potentialWant)
+                {
+                    if (el == null) isNull = true;
+                }
+                Console.WriteLine();
+
+                foreach (int? el in potentialHave)
+                {
+                    if (el == null) isNull = true;
+                }
+            } while (isNull);
+
+            string l = "";
+            foreach (int? el in potentialWant)
+            {
+                l += " " + el;
+            }
+            sw.WriteLine("Потенціали постачальників: " + l);
+            l = "";
+            foreach (int? el in potentialHave)
+            {
+               l += " " + el;
+            }
+            sw.WriteLine("Потенціали споживачів:: " + l);
+            bool isOpti = true;
+
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    if (potencials[i, j] == 0)
+                    {
+                        potencials[i, j] = (int)potentialWant[i] + (int)potentialHave[j];
+                        if (priceMatrix[i, j] < potencials[i, j])
+                        {
+                            isOpti = false;
+                        }
+
+                    }
+                }
+            }
+            string r = "";
+            sw.WriteLine("Непрямі вартості");
+            fileWrite(potencials, sizeX, sizeY);
+            if (isOpti)
+            {
+                sw.WriteLine("Розв'язок оптимальний");
+                return;
+            }
+
+                int changeStartX = -1;
+                int changeStartY = -1;
+                int dif = -1;
+                for (int i = 0; i < sizeX; i++)
+                {
+                    for (int j = 0; j < sizeY; j++)
+                    {
+                        if (potencials[i, j] != res[i, j])
+                        {
+                            if (potencials[i, j] > priceMatrix[i, j])
+                            {
+                                if(dif< potencials[i, j] - priceMatrix[i, j])
+                                {
+                                    changeStartX = i;
+                                    changeStartY = j;
+                                    dif = potencials[i, j] - priceMatrix[i, j];
+                                }
+                            }
+                        }
+                    }
+                }
+            sw.WriteLine($"Проблемна точка [{changeStartX},{changeStartY}]");
+            string[] equlense = textBoxQueue.Text.Split('\n');
+            string[] line = equlense[0].Split(' ');
+            int[,] changeMatrix = new int[line.Length + 1, 2];
+            changeMatrix[0,0]=changeStartX;
+            changeMatrix[0,1]=changeStartY;
+            for (int j = 0; j < equlense.Length; j++)
+            {
+                line = equlense[j].Split(' ');
+                for (int i = 0; i < line.Length; i++)
+                {
+                    changeMatrix[i+1, j] = int.Parse(line[i]);
+                }
+            }
+            int min=int.MaxValue;
+            for(int i = 1; i < line.Length - 1; i++)
+            {
+                if (potencials[changeMatrix[i, 0], changeMatrix[i, 1]] < min)
+                {
+                    min= potencials[changeMatrix[i, 0], changeMatrix[i, 1]];
+                }
+            }
+            sw.WriteLine("Знайдено значення λ: "+min);
+            dif = 1;
+            for(int i=0; i< line.Length+1; i++)
+            {
+                res[changeMatrix[i, 0], changeMatrix[i, 1]] += min * dif;
+                dif *= -1;
+            }
+
+        }
+
+  
+  
+    }
+}
